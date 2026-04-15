62207c2154f74aa1fb2e3b8a168e3625ff0b95d4
Jin Man Park <skswlsaks@gmail.com>
2026-04-15 13:54:16 +0000

    formula: source build, fix homepage/license for homebrew-core readiness
    - Switch from binary tarball to source build from GitHub archive
    - homepage: zeptodb.com (was zeptodb.github.io)
    - license: BSL-1.1 (was Apache-2.0)
    - Build deps: cmake, ninja, llvm@19, highway, openssl@3, lz4, readline
    - macOS + Linux support (was Linux-only)
    - Disable optional deps for minimal core build

--- Formula/zeptodb.rb
+++ Formula/zeptodb.rb
@@ -1,28 +1,46 @@
 class Zeptodb < Formula
   desc "Ultra-low latency in-memory time-series database"
-  homepage "https://zeptodb.github.io"
-  version "0.0.3"
-  license "Apache-2.0"
+  homepage "https://zeptodb.com"
+  url "https://github.com/ZeptoDB/ZeptoDB/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "a6e3c0184037163335c6a07b137e0efcb794ec93a6d2e2435ea5197b13be0829"
+  license "BSL-1.1"
 
-  on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/ZeptoDB/ZeptoDB/releases/download/v0.0.3/zeptodb-linux-amd64-0.0.3.tar.gz"
-      sha256 "f560635b6a46f1fea441e55a09e976ed9fe9878e4ee629325530f2d569203f0d"
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/ZeptoDB/ZeptoDB/releases/download/v0.0.3/zeptodb-linux-arm64-0.0.3.tar.gz"
-      sha256 "9dfaca1707bd7ec397c741129ed651b3f643a388a2b798e8169c358bc3140035"
-    end
-  end
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "llvm@19"
+  depends_on "highway"
+  depends_on "openssl@3"
+  depends_on "lz4"
+  depends_on "readline"
 
   def install
-    bin.install "zepto_http_server"
-    bin.install "zepto_data_node"
-    bin.install "zepto_flight_server"
-    bin.install "zepto-cli" => "zeptodb"
+    llvm = Formula["llvm@19"]
+
+    args = %W[
+      -GNinja
+      -DCMAKE_C_COMPILER=#{llvm.opt_bin}/clang
+      -DCMAKE_CXX_COMPILER=#{llvm.opt_bin}/clang++
+      -DZEPTO_BUILD_TESTS=OFF
+      -DZEPTO_BUILD_BENCH=OFF
+      -DZEPTO_USE_S3=OFF
+      -DZEPTO_USE_KAFKA=OFF
+      -DZEPTO_USE_FLIGHT=OFF
+      -DZEPTO_USE_PARQUET=OFF
+      -DZEPTO_USE_UCX=OFF
+      -DZEPTO_USE_HUGEPAGES=OFF
+      -DZEPTO_USE_TCMALLOC=OFF
+      -DZEPTO_USE_LTO=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "ninja", "-C", "build", "zepto-cli", "zepto_http_server", "zepto_data_node"
+
+    bin.install "build/tools/zepto-cli" => "zeptodb"
+    bin.install "build/tools/zepto_http_server"
+    bin.install "build/tools/zepto_data_node"
   end
 
   test do
-    assert_predicate bin/"zeptodb", :executable?
+    assert_match "ZeptoDB", shell_output("#{bin}/zeptodb --version 2>&1", 0)
   end
 end
